b3f5e0cf0810c95805e0022947754579a284653a
kallyfuu <134396430+kallyfuu@users.noreply.github.com>
2024-07-12 02:32:56 -0400

    Add files via upload
    comit final

--- angelaminimarketdjango1/app/forms.py
+++ angelaminimarketdjango1/app/forms.py
@@ -7,6 +7,11 @@ class ContactoForms(forms.ModelForm):
     class Meta:
         model = Contacto
         fields = '__all__'
+        widgets = {
+            'nombre': forms.TextInput(attrs={'id': 'id_nombre', 'required': False}),
+            'correo': forms.EmailInput(attrs={'id': 'id_correo', 'required': False}),
+            'mensaje': forms.Textarea(attrs={'id': 'id_mensaje', 'required': False}),
+        }
 
 class ProductoForms(forms.ModelForm):
 
--- angelaminimarketdjango1/app/static/app/js/validaciones.js
+++ angelaminimarketdjango1/app/static/app/js/validaciones.js
@@ -1,50 +1,51 @@
-document.getElementById('registroForm').addEventListener('submit', function(event) {
+document.getElementById('contactoForm').addEventListener('submit', function(event) {
   event.preventDefault(); // Evita que el formulario se envíe por defecto
 
   // Obtener los valores de los campos
-  var nombre = document.getElementById('nombre_login').value.trim();
-  var email = document.getElementById('email_login').value.trim();
-  var password = document.getElementById('password_login').value.trim();
+  var nombre = document.getElementById('id_nombre').value.trim();
+  var correo = document.getElementById('id_correo').value.trim();
+  var mensaje = document.getElementById('id_mensaje').value.trim();
 
   // Obtener las referencias de los elementos de error
   var nombreError = document.getElementById('nombre_error');
-  var emailError = document.getElementById('email_error');
-  var passwordError = document.getElementById('password_error');
+  var correoError = document.getElementById('correo_error');
+  var mensajeError = document.getElementById('mensaje_error');
 
   // Restablecer los mensajes de error
   resetErrorMessages();
 
-  // Validar nombre de usuario
+  // Validar nombre
   if (nombre === '') {
-      setError(nombreError, 'Por favor, ingresa tu nombre de usuario.');
+      setError(nombreError, 'Por favor, ingresa tu nombre.');
       return;
   }
 
   // Validar correo electrónico
-  if (email === '') {
-      setError(emailError, 'Por favor, ingresa tu correo electrónico.');
+  if (correo === '') {
+      setError(correoError, 'Por favor, ingresa tu correo electrónico.');
       return;
-  } else if (!isValidEmail(email)) {
-      setError(emailError, 'El formato del correo electrónico es incorrecto.');
+  } else if (!isValidEmail(correo)) {
+      setError(correoError, 'El formato del correo electrónico es incorrecto.');
       return;
   }
 
-  // Validar contraseña
-  if (password === '') {
-      setError(passwordError, 'Por favor, ingresa tu contraseña.');
+  // Validar mensaje
+  if (mensaje === '') {
+      setError(mensajeError, 'Por favor, ingresa tu mensaje.');
       return;
   }
 
   // Si todas las validaciones pasan, se puede proceder con el envío del formulario
-  alert("¡Usuario registrado exitosamente!");
+  alert("¡Formulario enviado exitosamente!");
   // Aquí puedes agregar código para enviar los datos del formulario a tu servidor
+  document.getElementById('contactoForm').submit();
 });
 
 // Función para restablecer los mensajes de error
 function resetErrorMessages() {
   document.getElementById('nombre_error').textContent = '';
-  document.getElementById('email_error').textContent = '';
-  document.getElementById('password_error').textContent = '';
+  document.getElementById('correo_error').textContent = '';
+  document.getElementById('mensaje_error').textContent = '';
 }
 
 // Función para establecer un mensaje de error
@@ -53,29 +54,7 @@ function setError(element, message) {
 }
 
 // Función para validar el formato del correo electrónico
-function isValidEmail(email) {
+function isValidEmail(correo) {
   const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
-  return emailRegex.test(email);
-}
-
-function validateForm() {
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-  var usernameError = document.getElementById('usernameError');
-  var passwordError = document.getElementById('passwordError');
-  usernameError.innerHTML = '';
-  passwordError.innerHTML = '';
-  var isValid = true;
-
-  if (!username) {
-    usernameError.innerHTML = 'Por favor, ingresa tu nombre de usuario';
-    isValid = false;
-  }
-
-  if (!password) {
-    passwordError.innerHTML = 'Por favor, ingresa tu contraseña';
-    isValid = false;
-  }
-
-  return isValid;
+  return emailRegex.test(correo);
 }
